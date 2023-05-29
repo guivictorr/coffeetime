87325ff4920dd011724b4135f04807a0b65b05d3
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2023-05-28 23:51:56 -0300

    chore: adding custom colors to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    colors: {
+      white: '#fff',
+      background: '#fafafa',
+      base: {
+        card: '#f3f2f2',
+        input: '#ededed',
+        button: '#e6e5e5',
+        hover: '#d7d5d5',
+        label: '#8d8686',
+        text: '#574f4d',
+        subtitle: '#403937',
+        title: '#272221',
+      },
+      purple: {
+        dark: '#4b2995',
+        base: '#8047f8',
+        light: '#ebe5f9',
+      },
+      yellow: {
+        dark: '#C47F17',
+        base: '#DBAC2C',
+        light: '#F1E9C9',
+      },
+    },
   },
   plugins: [],
 };
