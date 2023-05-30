15c9216e49e7a76915edc0bf6b89f8d4c17f4a1f
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2023-05-30 18:38:09 -0300

    refactor: adding type of input props for button select

--- src/components/button-select.tsx
+++ src/components/button-select.tsx
@@ -1,15 +1,24 @@
-import { ReactNode } from 'react';
+import { InputHTMLAttributes, ReactNode } from 'react';
 
 type ButtonSelectProps = {
   children: ReactNode;
-  id: string;
-  name: string;
-};
+} & InputHTMLAttributes<HTMLInputElement>;
 
-export function ButtonSelect({ children, id, name }: ButtonSelectProps) {
+export function ButtonSelect({
+  children,
+  id,
+  name,
+  ...rest
+}: ButtonSelectProps) {
   return (
     <div>
-      <input id={id} name={name} type='radio' className='peer hidden' />
+      <input
+        id={id}
+        name={name}
+        {...rest}
+        type='radio'
+        className='peer hidden'
+      />
       <label
         className='peer-checked:bg-purple-light peer-checked:border-purple cursor-pointer flex items-center justify-center gap-2 w-full transition bg-base-button border text-base-text hover:bg-base-hover border-base-button rounded-lg p-4'
         htmlFor={id}
