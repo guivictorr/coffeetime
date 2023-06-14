71fce45fd5879daee983d29bc1967af714ad6807
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2023-06-14 20:21:21 -0300

    feat: created home page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,12 +2,17 @@ import { lazy } from 'react';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
 const Root = lazy(() => import('../pages/root'));
+const Home = lazy(() => import('../pages/home'));
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Root />,
     children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
       {
         path: 'checkout',
         element: <h1>checkout</h1>,
