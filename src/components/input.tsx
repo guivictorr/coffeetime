da287fc9006f1823c1c844852806f071f2d973d4
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2023-06-17 01:22:32 -0300

    feat: adding address form to checkout page

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -2,10 +2,10 @@ import { InputHTMLAttributes } from 'react';
 
 export function Input({ ...rest }: InputHTMLAttributes<HTMLInputElement>) {
   return (
-    <div className='flex gap-1 items-center focus-within:border-yellow-dark rounded-[4px] bg-base-input p-3 border border-base-button'>
+    <div className='flex w-full gap-1 items-center text-sm sm:text-md focus-within:border-yellow-dark rounded-[4px] bg-base-input p-2 sm:p-3 border border-base-button'>
       <input
         {...rest}
-        className='peer text-base-text flex-1 bg-base-input focus:outline-none placeholder:text-base-label'
+        className='peer w-full text-base-text flex-1 bg-base-input focus:outline-none placeholder:text-base-label'
       />
       <span className='peer-required:hidden italic text-base-label'>
         Opcional
--- src/pages/checkout.tsx
+++ src/pages/checkout.tsx
@@ -1,3 +1,46 @@
+import { MapPinLine } from 'phosphor-react';
+import { Input } from '../components/input';
+
 export default function Checkout() {
-  return <h1>Checkout</h1>;
+  return (
+    <main className='grid grid-cols-1'>
+      <section className='bg-base-card p-4 rounded-md'>
+        <div>
+          <header className='flex gap-2'>
+            <MapPinLine className='h-5 w-5 text-yellow-dark' />
+            <div>
+              <p className='text-base-subtitle text-sm sm:text-md'>
+                Endereço de entrega
+              </p>
+              <p className='text-base-text text-xs sm:text-sm'>
+                Informe o endereço onde deseja receber seu pedido
+              </p>
+            </div>
+          </header>
+          <fieldset className='mt-6 sm:mt-8 space-y-2'>
+            <div className='sm:max-w-[200px]'>
+              <Input placeholder='CEP' required type='number' />
+            </div>
+            <Input placeholder='Rua' required type='text' />
+            <div className='flex flex-col sm:flex-row gap-2'>
+              <div className='w-full sm:max-w-[200px]'>
+                <Input placeholder='Número' required type='number' />
+              </div>
+              <Input placeholder='Complemento' type='number' />
+            </div>
+            <div className='flex items-center  gap-2'>
+              <div className='w-full sm:max-w-[200px]'>
+                <Input placeholder='Cidade' required type='text' />
+              </div>
+              <Input placeholder='Bairro' required type='text' />
+              <div className='max-w-[60px]'>
+                <Input placeholder='UF' required type='text' />
+              </div>
+            </div>
+          </fieldset>
+        </div>
+      </section>
+      <section className='bg-yellow'>carrinho</section>
+    </main>
+  );
 }
