3383d560845fa8d8a8647b522562213664567dfb
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2023-06-17 05:09:34 -0300

    feat: created cart component on checkout page

--- src/components/button-select.tsx
+++ src/components/button-select.tsx
@@ -20,7 +20,7 @@ export function ButtonSelect({
         className='peer hidden'
       />
       <label
-        className='text-sm leading-4 sm:text-md peer-checked:bg-purple-light peer-checked:border-purple cursor-pointer flex items-center justify-center gap-2 w-full transition bg-base-button border text-base-text hover:bg-base-hover border-base-button rounded-lg p-4'
+        className='text-sm leading-4 peer-checked:bg-purple-light peer-checked:border-purple cursor-pointer flex items-center justify-center gap-2 w-full transition bg-base-button border text-base-text hover:bg-base-hover border-base-button rounded-lg p-4'
         htmlFor={id}
       >
         {children}
--- src/pages/checkout.tsx
+++ src/pages/checkout.tsx
@@ -4,16 +4,19 @@ import {
   CurrencyDollar,
   MapPinLine,
   Money,
+  Trash,
 } from 'phosphor-react';
 import { Input } from '../components/input';
 import { ButtonSelect } from '../components/button-select';
+import { Counter } from '../components/counter';
+import { Button } from '../components/button';
 
 export default function Checkout() {
   return (
-    <main className='grid grid-cols-1 lg:gap-8 lg:grid-cols-[minmax(640px,_1fr)_1fr]'>
+    <main className='grid grid-cols-1 gap-8 lg:grid-cols-[minmax(340px,_1fr)_1fr] xl:grid-cols-[minmax(440px,_1fr)_1fr] pb-12'>
       <section>
         <h2 className='text-md mb-4'>Complete seu pedido</h2>
-        <div className='bg-base-card p-4 rounded-ss-md rounded-se-md sm:rounded-md'>
+        <article className='bg-base-card p-4 md:p-10 rounded-ss-md rounded-se-md sm:rounded-md'>
           <header className='flex gap-2'>
             <MapPinLine className='h-5 w-5 text-yellow-dark' />
             <div>
@@ -46,9 +49,9 @@ export default function Checkout() {
               </div>
             </div>
           </fieldset>
-        </div>
+        </article>
 
-        <div className='bg-base-card p-4 rounded-es-md rounded-ee-md sm:rounded-md sm:mt-3'>
+        <article className='bg-base-card p-4 md:p-10 rounded-es-md rounded-ee-md sm:rounded-md sm:mt-3'>
           <header className='flex gap-2'>
             <CurrencyDollar className='h-5 w-5 text-purple' />
             <div>
@@ -58,7 +61,7 @@ export default function Checkout() {
               </p>
             </div>
           </header>
-          <fieldset className='flex flex-col sm:flex-row gap-3 sm:justify-between mt-6 sm:mt-8'>
+          <fieldset className='flex flex-col 2xl:flex-row gap-3 sm:justify-between mt-6 sm:mt-8'>
             <ButtonSelect
               name='paymentMethod'
               value='credit-card'
@@ -80,11 +83,55 @@ export default function Checkout() {
               <span>Dinheiro</span>
             </ButtonSelect>
           </fieldset>
-        </div>
+        </article>
       </section>
       <section>
         <h3 className='text-md mb-4'>Cafés selecionados</h3>
-        <div className='bg-base-card'>teste</div>
+        <article className='p-4 md:p-10 bg-base-card rounded-se-3xl rounded-es-3xl rounded-ss-md rounded-ee-md'>
+          <ul>
+            <li className='flex'>
+              <img
+                src='src/assets/img/cream.png'
+                alt=''
+                className='hidden sm:block mr-5'
+              />
+              <div className=''>
+                <p className='mb-2'>Expresso tradicional</p>
+                <div className='flex gap-2'>
+                  <Counter onChange={console.log} />
+                  <Button variant='secondary'>
+                    <Trash className='text-purple' />
+                    <span>Remover</span>
+                  </Button>
+                </div>
+              </div>
+              <p className='grow text-end font-bold'>R$ 9,90</p>
+            </li>
+
+            <hr className='my-6 text-base-button' />
+          </ul>
+
+          <footer className='flex flex-col'>
+            <div className='flex justify-between mb-6'>
+              <div className='flex flex-col text-start gap-3'>
+                <span className='text-sm text-base-text'>Total de itens</span>
+                <span className='text-sm text-base-text'>Entrega</span>
+                <span className='text-base-subtitle text-lg font-bold'>
+                  Total
+                </span>
+              </div>
+              <div className='flex flex-col text-end gap-3'>
+                <span className='text-md text-base-text'>R$ 29,70</span>
+                <span className='text-md text-base-text'>R$ 3,50</span>
+                <span className='text-base-subtitle text-lg font-bold'>
+                  33,50
+                </span>
+              </div>
+            </div>
+
+            <Button>CONFIRMAR PEDIDO</Button>
+          </footer>
+        </article>
       </section>
     </main>
   );
